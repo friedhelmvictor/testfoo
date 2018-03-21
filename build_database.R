8d2543d7b079ac7bbaf7c0fbca2f143fae9fcf6b
Friedhelm Victor <friedhelm.victor@tu-berlin.de>
2018-03-21 14:46:48 +0100

    add database building and config logic

---
+++ build_database.R
@@ -0,0 +1,41 @@
+library(RPostgreSQL)
+
+con <- dbConnect(PostgreSQL(), host=config$dbConfig$host,
+                 user= config$dbConfig$user, password=config$dbConfig$password,
+                 dbname=config$dbConfig$dbname)
+
+(function() {
+  # Check if the tokenTransfers database may already exist with same number of rows
+  dbExists <- dbExistsTable(con, 'tokenTransfers')
+  hasSameRowCount <- tryCatch({
+    dbGetQuery(con, 'SELECT COUNT(*) AS count FROM "tokenTransfers"')$count == nrow(tokenTransfers)
+  }, error = function(e) { return(FALSE) }, warning = function(e) { return(FALSE) })
+  if(dbExists & hasSameRowCount) {
+    print('Same number of rows in database.')
+    print('Skipping database creation.')
+  } else {
+    # Otherwise delete remains and create the database anew
+    print(paste(Sys.time(), 'Creating database...'))
+    
+    dbRemoveTable(con, 'tokenTransfers')
+    dbWriteTable(con, 'tokenTransfers', tokenTransfers, row.names=F,
+                 field.types = list(address='VARCHAR(42)',
+                                    blockNumber='INT',
+                                    from='VARCHAR(42)',
+                                    to='VARCHAR(42)',
+                                    amount='DECIMAL(100,0)'))
+    print(paste(Sys.time(), 'Creating indices...'))
+    dbExecute(con, 'CREATE INDEX address_idx ON "tokenTransfers"(address);')
+    print(paste(Sys.time(), 'Finished.'))
+  }
+})()
+
+
+# TODO:
+# Extract to own file
+# Perform index creation
+# Create balances
+# Create indexes on balances
+# Introduce checks whether its really necessary to rebuild the database
+dbDisconnect(con)
+# dbGetQuery(mydb, 'PRAGMA table_info(mtcars);') # Get data types
--- computeFeatures.R
+++ computeFeatures.R
@@ -1,12 +1,25 @@
 library(data.table)
 
-tokenFileLocation <- "~/workspaces/data/ethereum/latest/tokenTransfers.csv"
-tokenTransfers <- fread(tokenFileLocation, colClasses = list("character"=c("address", "from", "to")))
+
+config <- list(tokenTransfersFile = '~/workspaces/data/ethereum/latest/tokenTransfers.csv',
+               dbConfig = list(host = 'localhost', user = 'postgres',
+                               password = 'postgres', dbname = 'postgres'),
+               createPlots = FALSE,
+               rows = 1000000) # -1 for all
+
+
+# TODO: Create config file with filename, and database details as well as what this script will build
+tokenTransfers <- fread(config$tokenTransfersFile,
+                        colClasses = list("character"=c("address", "from", "to", "amount")),
+                        nrows = config$rows)
 
 if(exists("tokenTransfers") && is.data.table(get("tokenTransfers"))) {
   
   # Clean the data
   source("clean_data.R")
+  
+  # Build balances database
+  source("build_database.R")
   
   # Create empty feature table
   featureTable <- data.table(address=unique(tokenTransfers$address))
